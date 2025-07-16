cbbe812f90da3524e9d744ed7020409a4ac1d44d
Richard <46233781+sweetbix@users.noreply.github.com>
2025-07-16 22:23:18 +1000

    Implemented tailwindcss

---
+++ src/components/TodoList.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState} from "react";
+
+function TodoList() {
+    const [todos, setTodos] = useState([]);
+
+    // Fetch todos on component mount
+    useEffect(() => {
+        fetch("/api/todos") // Hits express backend
+        .then((res) => res.json())
+        .then((data) => {
+            setTodos(data);
+        })
+        .catch((err) => console.log("Error fetching todos:", err));
+    }, []);
+
+    return (
+        <div className="p-4">
+            <h2 className="text-xl font-bold mb-4">My Todos</h2>
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo._id} className="mb-2">
+                        {todo.text}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default TodoList;
